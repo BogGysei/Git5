42e1a9e176d1fab9402a6cf1d0c8da4b26fb89ad
BogGysei <153365565+BogGysei@users.noreply.github.com>
2024-03-02 18:38:08 +0200

    Add files via upload

---
+++ Lab1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int h;
+	cout << "Введіть висоту піраміди: ";
+	cin >> h;
+	for (int i = 0; i < h; ++i) {
+		for (int j = 0; j < h - i - 1; ++j) {
+			cout << " ";
+		}
+		for (int j = 0; j < i + 1; ++j) {
+			cout << "*";
+		}
+		for (int j = 0; j < i; ++j) {
+			cout << j;
+		}
+		cout << '\n';
+	}
+}
